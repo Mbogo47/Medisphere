47dfd8c42b1550c092f4b959c1c6d45771064e38
Mbogo47 <97885622+Mbogo47@users.noreply.github.com>
2023-06-21 16:20:32 +0300

    Update vite.config.js

--- Front/vite.config.js
+++ Front/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
    build: {
     rollupOptions: {
-      external: ['react-pro-sidebar'],
+    external: ['react-pro-sidebar/dist/es'],
     },
   },
 })
